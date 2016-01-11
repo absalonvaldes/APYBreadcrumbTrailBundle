def8f3b15c0a179d35d7964d4b2b71c11e1a81e9
Absalón Valdés <absalon@bluecompany.cl>
2016-01-11 10:50:50 -0300

    Add RequestStack support. Symfony >= 2.4

--- BreadcrumbTrail/Trail.php
+++ BreadcrumbTrail/Trail.php
@@ -86,7 +86,8 @@ class Trail implements \IteratorAggregate, \Countable
                 throw new \InvalidArgumentException('The title of a breadcrumb must be a string.');
             }
 
-            $request = $this->container->get('request', ContainerInterface::NULL_ON_INVALID_REFERENCE);
+            $requestStack = $this->container->get('request_stack');
+            $request = $requestStack->getCurrentRequest();
 
             if ($request !== null) {
                 preg_match_all('#\{(?P<variable>\w+).?(?P<function>([\w\.])*):?(?P<parameters>(\w|,| )*)\}#', $breadcrumb_or_title, $matches, PREG_OFFSET_CAPTURE | PREG_SET_ORDER);
